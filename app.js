be016fa544fd3283959266660a61a4f1a309c5e9
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-21 15:23:58 -0600

    created package json

---
+++ app.js
@@ -0,0 +1,5 @@
+var express = require('express'); //npm init creates package.json file
+// terminal npm install express creates node modules folder
+// npm install --save - this puts the dependencies - don't need to install all node modules
+// they can now do npm install - which looks for json and dependencies (see json file)
+// 
